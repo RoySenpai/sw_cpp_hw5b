49f095340482af65289a64cf7f4bb5ceff756067
Roy <me@hiyorix.com>
2023-06-02 01:12:46 +0300

    Upload HW struct and solution

---
+++ sources/MagicalContainer.cpp
@@ -0,0 +1,119 @@
+/*
+ *  Software Systems CPP Course Assignment 5
+ *  Copyright (C) 2023  Roy Simanovich
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <memory>
+#include <algorithm>
+#include <stdexcept>
+#include "MagicalContainer.hpp"
+
+using namespace std;
+using namespace ariel;
+
+void MagicalContainer::addElement(int element) {
+	auto in = _elements.insert(element);
+
+	if (in.second)
+	{
+		const int* address = &(*in.first);
+
+		_elements_ascending_order.push_back(address);
+		
+		if (_isPrime(element))
+		{
+			_elements_prime_order.push_back(address);
+			sort(_elements_prime_order.begin(), _elements_prime_order.end(), [](const int* a, const int* b) { return *a < *b; });
+		}
+
+		sort(_elements_ascending_order.begin(), _elements_ascending_order.end(), [](const int* a, const int* b) { return *a < *b; });
+
+		_elements_sidecross_order.clear();
+		_elements_sidecross_order.reserve(_elements_ascending_order.size());
+
+		size_t start = 0, end = _elements_ascending_order.size() - 1;
+
+		if (_elements_ascending_order.size() == 1)
+			_elements_sidecross_order.push_back(_elements_ascending_order.at(0));
+
+		else
+		{
+			while (start <= end && end != 0)
+			{
+				_elements_sidecross_order.push_back(_elements_ascending_order.at(start));
+
+				if (start != end)
+					_elements_sidecross_order.push_back(_elements_ascending_order.at(end));
+
+				start++;
+				end--;
+			}
+		}
+	}
+}
+
+void MagicalContainer::removeElement(int element) {
+	auto it = _elements.find(element);
+
+	if (it == _elements.end())
+		throw std::runtime_error("Error: element not found");
+	
+	_elements.erase(element);
+
+	if (_isPrime(element))
+	{
+		auto it_prime = find(_elements_prime_order.begin(), _elements_prime_order.end(), &(*it));
+		_elements_prime_order.erase(it_prime);
+	}
+
+	auto it_ascending = find(_elements_ascending_order.begin(), _elements_ascending_order.end(), &(*it));
+	_elements_ascending_order.erase(it_ascending);
+
+	auto it_sidecross = find(_elements_sidecross_order.begin(), _elements_sidecross_order.end(), &(*it));
+	_elements_sidecross_order.erase(it_sidecross);
+}
+
+bool MagicalContainer::_isPrime(int num) {
+	if (abs(num) <= 1)
+		return false;
+	
+	for (int i = 2; i < num; ++i)
+	{
+		if (num % i == 0)
+			return false;
+	}
+	
+	return true;
+}
+
+MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer& container): _container(container), _index(0) {}
+
+MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer& container, size_t index): _container(container), _index(index) {}
+
+MagicalContainer::AscendingIterator::AscendingIterator(const AscendingIterator &other): _container(other._container), _index(other._index) {}
+
+MagicalContainer::SideCrossIterator::SideCrossIterator(MagicalContainer& container): _container(container), _index(0) {}
+
+MagicalContainer::SideCrossIterator::SideCrossIterator(MagicalContainer& container, size_t index): _container(container), _index(index) {}
+
+MagicalContainer::SideCrossIterator::SideCrossIterator(const SideCrossIterator &other): _container(other._container), _index(other._index) {}
+
+MagicalContainer::PrimeIterator::PrimeIterator(MagicalContainer& container): _container(container), _index(0) {}
+
+MagicalContainer::PrimeIterator::PrimeIterator(MagicalContainer& container, size_t index): _container(container), _index(index) {}
+
+MagicalContainer::PrimeIterator::PrimeIterator(const PrimeIterator &other): _container(other._container), _index(other._index) {}
---
+++ sources/MagicalContainer.hpp
@@ -0,0 +1,532 @@
+/*
+ *  Software Systems CPP Course Assignment 5
+ *  Copyright (C) 2023  Roy Simanovich
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <set>
+#include <vector>
+#include <stdexcept>
+
+namespace ariel
+{
+	class MagicalContainer
+	{
+		private:
+			std::set<int> _elements;
+			std::vector<const int *> _elements_ascending_order;
+			std::vector<const int *> _elements_sidecross_order;
+			std::vector<const int *> _elements_prime_order;
+
+			/*
+			 * @brief Checks if a given number is prime.
+			 * @param num The number to check.
+			 * @return True if the number is prime, false otherwise.
+			*/
+			static bool _isPrime(int num);
+
+		public:
+			/*
+			 * @brief Construct a new Magical Container object.
+			 * @note The container is empty by default.
+			*/
+			MagicalContainer() { }
+
+			/*
+			 * @brief Add an element to the container.
+			 * @param element The element to add.
+			 * @note The element is added to the end of the container.
+			 * @note If the element already exists in the container, it will not be added.
+			*/
+			void addElement(int element);
+
+			/*
+			 * @brief Remove an element from the container.
+			 * @param element The element to remove.
+			 * @throw std::invalid_argument If the element does not exist in the container.
+			*/
+			void removeElement(int element);
+
+			/*
+			 * @brief Return the size of the container.
+			 * @return The size of the container.
+			*/
+			size_t size() const {
+				return _elements.size();
+			}
+
+
+
+		class AscendingIterator
+		{
+			private:
+				MagicalContainer& _container;
+				size_t _index;
+
+				/*
+				 * @brief Construct a new Ascending Iterator object.
+				 * @param container The container to iterate over.
+				 * @param index The index to start iterating from.
+				 * @note The iterator is initialized to the element at the given index in the container.
+				*/
+				AscendingIterator(MagicalContainer &container, size_t index);
+
+			public:
+				/*
+				 * @brief Construct a new Ascending Iterator object.
+				 * @param container The container to iterate over.
+				 * @note The iterator is initialized to the first element in the container.
+				*/
+				AscendingIterator(MagicalContainer &container);
+
+				/*
+				 * @brief Destroy the Ascending Iterator object.
+				 * @note Use the default destructor, as the container property is a vector, which has its own destructor.
+				*/
+				~AscendingIterator() = default;
+
+				/*
+				 * @brief Copy constructor.
+				 * @param other The iterator to copy.
+				*/
+				AscendingIterator(const AscendingIterator &other);
+
+				/*
+				 * @brief Move constructor.
+				 * @param other The iterator to move.
+				 * @note The other iterator is left in a valid but unspecified state.
+				 * @note Use the default move constructor, as the container property is a vector, which has its own move constructor.
+				*/
+				AscendingIterator(AscendingIterator &&other) noexcept;
+				
+				/*
+				 * @brief Copy assignment operator.
+				 * @param other The iterator to copy.
+				 * @return A reference to this iterator.
+				*/
+				AscendingIterator &operator=(const AscendingIterator &other) {
+					if (this != &other)
+					{
+						if (&_container != &other._container)
+						{
+							throw std::runtime_error("Cannot assign iterators from different containers");
+						}
+
+						_index = other._index;
+					}
+
+					return *this;
+				}
+
+				/*
+				 * @brief Move assignment operator.
+				 * @param other The iterator to move.
+				 * @return A reference to this iterator.
+				 * @note The other iterator is left in a valid but unspecified state.
+				 * @note Use the default move assignment operator, as the container property is a vector, which has its own move assignment operator.
+				*/
+				AscendingIterator &operator=(AscendingIterator &&other) noexcept;
+			
+				/*
+				 * @brief Equality operator, checks if two iterators are equal by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if the iterators are equal, false otherwise.
+				*/
+				bool operator==(const AscendingIterator &other) const {
+					return _index == other._index;
+				}
+
+				/*
+				 * @brief Inequality operator, checks if two iterators are not equal by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if the iterators are not equal, false otherwise.
+				*/
+				bool operator!=(const AscendingIterator &other) const {
+					return _index != other._index;
+				}
+
+				/*
+				 * @brief Less than operator, checks if this iterator is less than the other iterator by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if this iterator is less than the other iterator, false otherwise.
+				*/
+				bool operator<(const AscendingIterator &other) const {
+					return _index < other._index;
+				}
+
+				/*
+				 * @brief Greater than operator, checks if this iterator is greater than the other iterator by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if this iterator is greater than the other iterator, false otherwise.
+				*/
+				bool operator>(const AscendingIterator &other) const {
+					return _index > other._index;
+				}
+
+				/*
+				 * @brief Dereference operator, returns the element at the current index.
+				 * @return The element at the current index (If the index is out of bounds, the behavior is undefined).
+				*/
+				int operator*() const {
+					if (_index > _container._elements_ascending_order.size())
+					{
+						throw std::out_of_range("Iterator out of range");
+					}
+
+					return *(_container._elements_ascending_order.at(_index));
+				}
+
+				/*
+				 * @brief Prefix increment operator, increments the iterator to the next element.
+				 * @return A reference to this iterator.
+				 * @throw std::runtime_error If the iterator is out of range.
+				*/
+				AscendingIterator &operator++() {
+					if (_index >= _container._elements_ascending_order.size())
+					{
+						throw std::runtime_error("Iterator out of range");
+					}
+					
+					++_index;
+					return *this;
+				}
+
+				/*
+				 * @brief Returns an iterator to the first element in the container.
+				 * @return An iterator to the first element in the container.
+				 * @note If the container is empty, the iterator returned is equal to the iterator returned by end().
+				*/
+				AscendingIterator begin() {
+					return AscendingIterator(_container, 0);
+				}
+
+				/*
+				 * @brief Returns an iterator to the element after the last element in the container.
+				 * @return An iterator to the element after the last element in the container.
+				 * @note This iterator is not dereferenceable.
+				*/
+				AscendingIterator end() const {
+					return AscendingIterator(_container, _container._elements_ascending_order.size());
+				}
+		};
+
+		class PrimeIterator
+		{
+			private:
+				MagicalContainer& _container;
+				size_t _index;
+
+				/*
+				 * @brief Construct a new Prime Iterator object, with a given index.
+				 * @param container The container to iterate over.
+				 * @param index The index to initialize the iterator to.
+				 * @note The iterator is initialized to the given index.
+				*/
+				PrimeIterator(MagicalContainer &container, size_t index);
+
+			public:
+				/*
+				 * @brief Construct a new Prime Iterator object.
+				 * @param container The container to iterate over.
+				 * @note The iterator is initialized to the first element in the container.
+				*/
+				PrimeIterator(MagicalContainer &container);
+
+				/*
+				 * @brief Destroy the Prime Iterator object.
+				 * @note Use the default destructor, as the container property is a vector, which has its own destructor.
+				*/
+				~PrimeIterator() = default;
+
+				/*
+				 * @brief Copy constructor.
+				 * @param other The iterator to copy.
+				*/
+				PrimeIterator(const PrimeIterator &other);
+
+				/*
+				 * @brief Move constructor.
+				 * @param other The iterator to move.
+				 * @note The other iterator is left in a valid but unspecified state.
+				 * @note Use the default move constructor, as the container property is a vector, which has its own move constructor.
+				*/
+				PrimeIterator(PrimeIterator &&other) noexcept;
+				
+				/*
+				 * @brief Copy assignment operator.
+				 * @param other The iterator to copy.
+				 * @return A reference to this iterator.
+				*/
+				PrimeIterator &operator=(const PrimeIterator &other) {
+					if (this != &other)
+					{
+						if (&_container != &other._container)
+						{
+							throw std::runtime_error("Cannot assign iterators from different containers");
+						} 
+
+						_index = other._index;
+					}
+
+					return *this;
+				}
+
+				/*
+				 * @brief Move assignment operator.
+				 * @param other The iterator to move.
+				 * @return A reference to this iterator.
+				 * @note The other iterator is left in a valid but unspecified state.
+				 * @note Use the default move assignment operator, as the container property is a vector, which has its own move assignment operator.
+				*/
+				PrimeIterator &operator=(PrimeIterator &&other) noexcept;
+			
+				/*
+				 * @brief Equality operator, checks if two iterators are equal by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if the iterators are equal, false otherwise.
+				*/
+				bool operator==(const PrimeIterator &other) const {
+					return _index == other._index;
+				}
+
+				/*
+				 * @brief Inequality operator, checks if two iterators are not equal by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if the iterators are not equal, false otherwise.
+				*/
+				bool operator!=(const PrimeIterator &other) const {
+					return _index != other._index;
+				}
+
+				/*
+				 * @brief Less than operator, checks if this iterator is less than the other iterator by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if this iterator is less than the other iterator, false otherwise.
+				*/
+				bool operator<(const PrimeIterator &other) const {
+					return _index < other._index;
+				}
+
+				/*
+				 * @brief Greater than operator, checks if this iterator is greater than the other iterator by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if this iterator is greater than the other iterator, false otherwise.
+				*/
+				bool operator>(const PrimeIterator &other) const {
+					return _index > other._index;
+				}
+
+				/*
+				 * @brief Dereference operator, returns the element at the current index.
+				 * @return The element at the current index (If the index is out of bounds, the behavior is undefined).
+				*/
+				int operator*() const {
+					if (_index > _container._elements_ascending_order.size())
+					{
+						throw std::out_of_range("Iterator out of range");
+					}
+
+					return *(_container._elements_prime_order.at(_index));
+				}
+
+				/*
+				 * @brief Prefix increment operator, increments the iterator to the next element.
+				 * @return A reference to this iterator.
+				 * @throw std::runtime_error If the iterator is out of range.
+				*/
+				PrimeIterator &operator++() {
+					if (_index >= _container._elements_prime_order.size())
+					{
+						throw std::runtime_error("Iterator out of range");
+					}
+					
+					++_index;
+					return *this;
+				}
+
+				/*
+				 * @brief Returns an iterator to the first element in the container.
+				 * @return An iterator to the first element in the container.
+				 * @note If the container is empty, the iterator returned is equal to the iterator returned by end().
+				*/
+				PrimeIterator begin() {
+					return PrimeIterator(_container, 0);
+				}
+
+				/*
+				 * @brief Returns an iterator to the element after the last element in the container.
+				 * @return An iterator to the element after the last element in the container.
+				 * @note This iterator is not dereferenceable.
+				*/
+				PrimeIterator end() const {
+					return PrimeIterator(_container, _container._elements_prime_order.size());
+				}
+		};
+
+		class SideCrossIterator
+		{
+			private:
+				MagicalContainer& _container;
+				size_t _index;
+
+				/*
+				 * @brief Construct a new Side Cross Iterator object, with a given index.
+				 * @param container The container to iterate over.
+				 * @param index The index to initialize the iterator to.
+				 * @note The iterator is initialized to the given index.
+				*/
+				SideCrossIterator(MagicalContainer &container, size_t index);
+
+			public:
+				/*
+				 * @brief Construct a new Side Cross Iterator object.
+				 * @param cont The container to iterate over.
+				 * @note The iterator is initialized to the first element in the container.
+				*/
+				SideCrossIterator(MagicalContainer &container);
+
+				/*
+				 * @brief Destroy the Side Cross Iterator object.
+				 * @note Use the default destructor, as the container property is a vector, which has its own destructor.
+				*/
+				~SideCrossIterator() = default;
+
+				/*
+				 * @brief Copy constructor.
+				 * @param other The iterator to copy.
+				*/
+				SideCrossIterator(const SideCrossIterator &other);
+
+				/*
+				 * @brief Move constructor.
+				 * @param other The iterator to move.
+				 * @note The other iterator is left in a valid but unspecified state.
+				 * @note Use the default move constructor, as the container property is a vector, which has its own move constructor.
+				*/
+				SideCrossIterator(SideCrossIterator &&other) noexcept;
+				
+				/*
+				 * @brief Copy assignment operator.
+				 * @param other The iterator to copy.
+				 * @return A reference to this iterator.
+				*/
+				SideCrossIterator &operator=(const SideCrossIterator &other) {
+					if (this != &other)
+					{
+						if (&_container != &other._container)
+						{
+							throw std::runtime_error("Cannot assign iterators from different containers");
+						}
+
+						_index = other._index;
+					}
+
+					return *this;
+				}
+
+				/*
+				 * @brief Move assignment operator.
+				 * @param other The iterator to move.
+				 * @return A reference to this iterator.
+				 * @note The other iterator is left in a valid but unspecified state.
+				 * @note Use the default move assignment operator, as the container property is a vector, which has its own move assignment operator.
+				*/
+				SideCrossIterator &operator=(SideCrossIterator &&other) noexcept;
+			
+				/*
+				 * @brief Equality operator, checks if two iterators are equal by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if the iterators are equal, false otherwise.
+				*/
+				bool operator==(const SideCrossIterator &other) const {
+					return _index == other._index;
+				}
+
+				/*
+				 * @brief Inequality operator, checks if two iterators are not equal by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if the iterators are not equal, false otherwise.
+				*/
+				bool operator!=(const SideCrossIterator &other) const {
+					return _index != other._index;
+				}
+
+				/*
+				 * @brief Less than operator, checks if this iterator is less than the other iterator by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if this iterator is less than the other iterator, false otherwise.
+				*/
+				bool operator<(const SideCrossIterator &other) const {
+					return _index < other._index;
+				}
+
+				/*
+				 * @brief Greater than operator, checks if this iterator is greater than the other iterator by comparing their index.
+				 * @param other The iterator to compare to.
+				 * @return True if this iterator is greater than the other iterator, false otherwise.
+				*/
+				bool operator>(const SideCrossIterator &other) const {
+					return _index > other._index;
+				}
+
+				/*
+				 * @brief Dereference operator, returns the element at the current index.
+				 * @return The element at the current index (If the index is out of bounds, the behavior is undefined).
+				*/
+				int operator*() const {
+					if (_index > _container._elements_ascending_order.size())
+					{
+						throw std::out_of_range("Iterator out of range");
+					}
+					
+					return *(_container._elements_sidecross_order.at(_index));
+				}
+
+				/*
+				 * @brief Prefix increment operator, increments the iterator to the next element.
+				 * @return A reference to this iterator.
+				 * @throw std::runtime_error If the iterator is out of range.
+				*/
+				SideCrossIterator &operator++() {
+					if (_index >= _container._elements_sidecross_order.size())
+					{
+						throw std::runtime_error("Iterator out of range");
+					}
+					
+					++_index;
+					return *this;
+				}
+
+				/*
+				 * @brief Returns an iterator to the first element in the container.
+				 * @return An iterator to the first element in the container.
+				 * @note If the container is empty, the iterator returned is equal to the iterator returned by end().
+				*/
+				SideCrossIterator begin() {
+					return SideCrossIterator(_container, 0);
+				}
+
+				/*
+				 * @brief Returns an iterator to the element after the last element in the container.
+				 * @return An iterator to the element after the last element in the container.
+				 * @note This iterator is not dereferenceable.
+				*/
+				SideCrossIterator end() const {
+					return SideCrossIterator(_container, _container._elements_sidecross_order.size());
+				}
+		};
+	};
+}
